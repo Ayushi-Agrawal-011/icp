8bfa9cd59efeef9a8c205181fe82a0a8fe77dfb0
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-09-29 21:36:40 +0530

    Add files via upload

---
+++ assignment3/Number_Island.java
@@ -0,0 +1,32 @@
+package home_ass;
+
+public class Number_Island {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	  public int numIslands(char[][] grid) {
+	        int count=0;
+	        for(int i=0;i<grid.length;i++){
+	            for(int j=0;j<grid[i].length;j++){
+	                if(grid[i][j]=='1'){
+	                    isitsafe(grid,i,j);
+	                    count++;
+	                }
+	            }
+	        }
+	        return count;
+	    }
+	    public static void isitsafe(char[][]grid,int i,int j){
+	        if(i<0 || j<0 || i>=grid.length || j>=grid[i].length|| grid[i][j]=='0')
+	         return ;
+	             grid[i][j] = '0';
+	         isitsafe(grid,i+1,j);
+	        isitsafe(grid,i-1,j); 
+	        isitsafe(grid,i,j+1);
+	        isitsafe(grid,i,j-1);
+	        
+	        return;
+	    }
+}
