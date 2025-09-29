371d04d9853cd7e04ba9d574fee94ca2a633db52
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-09-29 21:17:58 +0530

    Add files via upload

---
+++ assignment1/sliding_window.java
@@ -0,0 +1,30 @@
+package home_ass;
+import java.util.*;
+public class sliding_window {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = {1,3,-1,-3,5,3,6,7};
+	}
+	 public int[] maxSlidingWindow(int[] arr, int k) {
+         int n = arr.length;
+        int[] result = new int[n - k + 1];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        for(int i=0;i<k;i++) {
+	  pq.offer(new int[]{arr[i], i});
+	}
+      result[0] = pq.peek()[0];
+    for (int i = k; i < n; i++) {
+            pq.offer(new int[]{arr[i], i});
+            
+            // remove elements outside the window
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            
+            // max of current window
+            result[i - k + 1] = pq.peek()[0];
+        }
+        return result;
+    }
+}
