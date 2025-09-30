321e2403accb431b1f2bafdd9f804f616c3b913c
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-09-30 21:14:23 +0530

    Add files via upload

---
+++ assignment4/Character_Replacement.java
@@ -0,0 +1,35 @@
+package home_ass;
+
+public class Character_Replacement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public int characterReplacement(String s, int k) {
+        int maxLen = 0;
+       for (char ch = 'A'; ch <= 'Z'; ch++) {
+           maxLen = Math.max(maxLen, Maxlen(s, ch, k));
+       }
+       return maxLen;
+   
+}
+
+   public static int Maxlen(String s,char ch,int k) {
+	int si=0,ei=0,flip=0,ans=0;
+	while(ei<s.length())
+	{if(s.charAt(ei)!=ch)
+		flip++;
+	while(flip>k && si<=ei) {
+		if(s.charAt(si)!=ch) {
+			flip--;
+		}
+		si++;
+	}
+	ans=Math.max(ans, ei-si+1);
+	//System.out.println(ans);
+	ei++;
+	}
+	return ans;
+}
+}
