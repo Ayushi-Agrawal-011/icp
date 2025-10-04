e3bc283de8355076d01749cf29a45e898598cb21
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-04 20:42:19 +0530

    Add files via upload

---
+++ assignment7/Maximum_Subarray.java
@@ -0,0 +1,21 @@
+package home_ass;
+
+public class Maximum_Subarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	 public int maxSubArray(int[] arr) {
+	        int ans=Integer.MIN_VALUE;
+		    	int sum=0;
+		    	for(int i=0;i<arr.length;i++) {
+		    		sum+=arr[i];
+	    			ans=Math.max(ans, sum);
+	    			if(sum<0) {
+	    				sum=0;
+	    			}
+		    	}
+		    	return ans;
+	    }
+}
