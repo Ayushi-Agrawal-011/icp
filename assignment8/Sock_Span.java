8f37e0220c235cbfefe6848de01c0c28faa9bd28
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-06 20:41:28 +0530

    Add files via upload

---
+++ assignment8/Sock_Span.java
@@ -0,0 +1,17 @@
+package home_ass;
+import java.util.*;
+public class Sock_Span {
+	Stack<Pair<Integer,Integer>> st;
+    public StockSpanner() {
+      st = new Stack<>();   
+    }
+    
+    public int next(int price) {
+         int cnt = 1;
+        while(!st.isEmpty() && st.peek().getKey() <= price){
+            cnt += st.pop().getValue();
+        }
+        st.push(new Pair(price, cnt));
+        return cnt;
+    }
+}
